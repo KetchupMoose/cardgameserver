ef3c74b6481029f950ed282cbf765bd32d146d0c
BrianAllen <brianallenmarketing@gmail.com>
2016-10-26 22:11:18 -0700

    4rdCommit

--- cloud/main.js
+++ cloud/main.js
@@ -1,3 +1,34 @@
-Parse.Cloud.define('hello', function(req, res) {
-  res.success('Hi');
+/**
+* Not actually any secure, but having function in cloud prevents errors in updating.
+* In future can store level costs in cloud code
+*
+* levelID - string
+* gold - integer
+* blankCards - integer
+*
+*/
+
+
+
+Parse.Cloud.define("levelComplete", function(request, response) {
+var completedLevels = request.user.get("completedLevels");
+
+if (completedLevels.indexOf(request.params.levelID) == -1)
+{
+request.user.increment("gold", request.params.gold);
+request.user.increment("blankCards", request.params.goldReward);
+completedLevels.push(request.params.levelID);
+
+request.user.save({},{
+success: function() {
+response.success();
+},
+error: function(error) {
+response.error("Couldn't save");
+}
+});
+}
+else{
+response.error("Level already completed");
+}
 });
