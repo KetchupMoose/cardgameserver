b49313c01cafdc5d5afa70330d1064a1e7207262
BrianAllen <brianallenmarketing@gmail.com>
2016-11-26 13:21:16 -0800

    nov 26 changes

--- cloud/main.js
+++ cloud/main.js
@@ -91,3 +91,62 @@ response.error("Couldn't find card");
 }
 })
 });
+
+/**
+* Publishes the card
+* cardID - Card's objectID
+testing new commit Nov 26 fixing function
+*/
+Parse.Cloud.define("publishCard", function(request, response) {
+if (request.user.get("blankCards") <= 0)
+{
+response.error("No blank cards left");
+return;
+}
+
+var cardQuery = new Parse.Query("Card");
+cardQuery.get(request.params.cardID, {
+success:function(card)
+{
+var databaseQuery = new Parse.Query("Database");
+databaseQuery.first({
+success: function(database) {
+//give the card an ID
+var idNumber = database.get("cardIdCounter");
+database.increment("cardIdCounter");
+card.set("idNumber", idNumber);
+
+var sale = new Parse.Object("Sale");
+sale.set("cardID", idNumber);
+sale.set("likes", 0);
+sale.set("seller", card.get("creator"));
+sale.set("stock", 10);
+sale.set("card", card);
+sale.set("name", card.get("name"));
+sale.set("tags", card.get("tags"));
+
+//TODO go through each tag and increment tag counters
+
+request.user.increment("blankCards", -1);
+setOwnedCard(request.user, idNumber, true);
+
+Parse.Object.saveAll([request.user, card, database, sale], {
+success: function(list) {
+//assumes all are saved
+response.success();
+},
+error: function(error) {
+response.error("Couldn't save");
+}
+});
+},
+error: function() {
+response.error(-1);
+}
+});
+},
+error: function() {
+response.error("Couldn't find card");
+}
+})
+});
